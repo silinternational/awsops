256a76f256d9c3b090d50d8e696744accc1b8316
Phillip Shipley <phillip.shipley@gmail.com>
2018-08-23 13:22:56 -0400

    change codeship setup

--- codeship/setup.sh
+++ codeship/setup.sh
@@ -3,12 +3,6 @@
 # exit if any command fails
 set -e
 
-# set NPM cache
-npm config set cache "${HOME}/cache/npm/"
-export PATH="${HOME}/cache/npm/bin/:${PATH}"
-export PREFIX="${HOME}/cache/npm/"
-nvm install 9
-
 # Upgrade Go to 1.10.3
 GO_VERSION=1.10.3
 source /dev/stdin <<< "$(curl -sSL https://raw.githubusercontent.com/codeship/scripts/master/languages/go.sh)"
