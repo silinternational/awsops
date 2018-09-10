6e2df586344c8d7954f1866a0dac0a2e254ff415
Phillip Shipley <phillip.shipley@gmail.com>
2018-09-10 16:01:52 -0400

    update usage statement

--- cmd/lambda.go
+++ cmd/lambda.go
@@ -21,13 +21,8 @@ import (
 // lambdaCmd represents the lambda command
 var lambdaCmd = &cobra.Command{
 	Use:   "lambda",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Commands for interacting with Lambda service",
+	Long:  "Commands for interacting with Lambda service",
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
