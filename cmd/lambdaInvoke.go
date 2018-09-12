5436b453e4bd79cffc3c244215e1004e9a9db832
Phillip Shipley <phillip.shipley@gmail.com>
2018-09-12 16:56:50 -0400

    fix command description

--- cmd/lambdaInvoke.go
+++ cmd/lambdaInvoke.go
@@ -28,12 +28,7 @@ var payload string
 var invokeCmd = &cobra.Command{
 	Use:   "invoke",
 	Short: "Invoke a lambda function",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  "Invoke a lambda function",
 	Run: func(cmd *cobra.Command, args []string) {
 		initAwsSess()
 
