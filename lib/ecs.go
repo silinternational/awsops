6b5b16d1ddaec61c8816f6e664356af9b560bf4a
Phillip Shipley <phillip.shipley@gmail.com>
2018-10-01 16:33:28 -0400

    removed unused import

--- lib/ecs.go
+++ lib/ecs.go
@@ -3,7 +3,6 @@ package lib
 import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/ecs"
