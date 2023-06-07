620463c47cbd3aa423c54599a69c2c5003386564
briskt <3172830+briskt@users.noreply.github.com>
2023-06-07 15:41:01 +0800

    Test min function

--- lib/ecs_test.go
+++ lib/ecs_test.go
@@ -15,3 +15,12 @@ func Test_max(t *testing.T) {
 	require.Equal(t, max(math.MaxInt, -2), int64(math.MaxInt))
 	require.Equal(t, max(math.MinInt, -2), int64(-2))
 }
+
+func Test_min(t *testing.T) {
+	require.Equal(t, min(2, 1), int64(1))
+	require.Equal(t, min(1, 2), int64(1))
+	require.Equal(t, min(-2, 1), int64(-2))
+	require.Equal(t, min(1, -2), int64(-2))
+	require.Equal(t, min(math.MaxInt, -2), int64(-2))
+	require.Equal(t, min(math.MinInt, -2), int64(math.MinInt))
+}
